c6a18a9d9e24700fd37116c8c2e2a5ac9e1136ae
stevie-steve <sharpsrjr@gmail.com>
2020-05-15 17:37:44 -0600

    connect JPA project dep

---
+++ BoondockApp/src/main/java/com/skilldistillery/boondock/BoondockAppApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.boondock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoondockAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoondockAppApplication.class, args);
+	}
+
+}
