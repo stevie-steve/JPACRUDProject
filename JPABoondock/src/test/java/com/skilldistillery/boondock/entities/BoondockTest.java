a2cc1c79b556d53878cfff52de3d75cb3aa4f289
stevie-steve <sharpsrjr@gmail.com>
2020-05-15 17:17:21 -0600

    stub jpa projecty, girtaffe entity and junit test

---
+++ JPABoondock/src/test/java/com/skilldistillery/boondock/entities/BoondockTest.java
@@ -0,0 +1,49 @@
+package com.skilldistillery.boondock.entities;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BoondockTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Boondock boondock;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("BoondockPU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		boondock = em.find(Boondock.class, 1);
+
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		boondock = null;
+	}
+
+	@Test
+	void test() {
+assertNotNull("", boondock.getName());	}
+
+}
